22beeb3eb1aaa2d05f57c2b24537ffaec0d8d376
Sourabh <kalelsaurabh11@gmail.com>
2024-06-26 11:25:14 +0530

    Fix reminder jobs deletion and order tasks by deadlines

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.asc(:deadline)
   end
 
   def show
--- app/workers/reminder_deletion_job.rb
+++ app/workers/reminder_deletion_job.rb
@@ -3,11 +3,10 @@ require 'sidekiq/api'
 class ReminderDeletionJob
   include Sidekiq::Worker
 
+  # Retrieve the task and delete associated reminder jobs
   def perform(task_id)
-    # Retrieve the task and delete associated reminder jobs
-    task = Task.where(id: task_id).first
     jobs_to_delete = Sidekiq::ScheduledSet.new.select do |job|
-      job.args.first == task.id && job.queue == 'default' && job.klass == 'ReminderJob'
+      job.args.second == task_id && job.queue == 'default' && job.klass == 'ReminderJob'
     end
 
     jobs_to_delete.each(&:delete)
